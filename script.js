719e271658e1e9a972ecca79ac385036754ce1ec
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-04-03 19:00:37 +0530

    Updating : script.js file

--- script.js
+++ script.js
@@ -17,4 +17,20 @@ recognition.onstart = function () {
 //Event listener for when speech recognition results are available
 recognition.onresult = function(event){
 const transcript = event.results[event.results.length - 1][0].transcript ; //Get the transcript of the last result
+output.textContent = transcript;
 }
+
+// Event listener for errors
+recognition.onerror = function(){
+    output.textContent = 'Error Occured : ' + event.error;
+};
+
+// Event listener for when speech recognition ends
+recognition.onend = function(){
+    output.textContent = 'Speech recognition ended';
+};
+
+// Event listener for the start button click
+startButton.addEventListener('click' , ()=>{
+    recognition.start(); //Start speech recogntion
+})
